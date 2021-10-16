a9bf01382c12a962eb31ae69696d6d569b24810f
Vladislav <timofeev.vladislav.sergeevich@gmail.com>
2021-10-17 00:31:32 +0300

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println("Загадка : Что это такое: синий, большой, с усами и полностью набит зайцами?");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите ваш ответ: ");
-        String answer = scanner.nextLine();
 
-        switch (answer.toLowerCase()){
-            case "троллейбус":
-                System.out.println("Правильно!");
+        boolean isCompleted = false;
+
+        for (int i = 0; i < 3; i++) {
+            if (isCompleted) {
                 break;
-            case "сдаюсь":
-                System.out.println("Правильный ответ : Троллейбус");
-                break;
-            default:
-                System.out.println("Подумай еще.");
+            }
+            String answer = scanner.nextLine();
+            switch (answer.toLowerCase()) {
+                case "троллейбус":
+                    System.out.println("Правильно!");
+                    isCompleted = true;
+                    break;
+                case "сдаюсь":
+                    System.out.println("Правильный ответ : Троллейбус");
+                    break;
+                default:
+                    System.out.println("Подумай еще.");
+            }
         }
     }
+
 }
