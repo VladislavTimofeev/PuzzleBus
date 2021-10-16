22ca97d41344dc16b216637946fd088a3f6a5105
Vladislav <timofeev.vladislav.sergeevich@gmail.com>
2021-10-16 23:38:50 +0300

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваш ответ: ");
+        String answer = scanner.nextLine();
+
+        switch (answer.toLowerCase()){
+            case "троллейбус":
+                System.out.println("Правильно!");
+                break;
+            case "сдаюсь":
+                System.out.println("Правильный ответ : Троллейбус");
+                break;
+            default:
+                System.out.println("Подумай еще.");
+        }
     }
 }
